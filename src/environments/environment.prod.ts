f61e8c1feb20f10b65b286759bf6c1cc4b70f696
alinemorelli <aline.morelliw@gmail.com>
2018-07-25 17:03:48 -0300

    fix: change redirect url to use fuzzer.app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   spotify: {
     clientId: 'b69d4477ec644c22a999afc386cc74b6',
     scopes: 'user-library-read user-top-read user-read-email playlist-modify-private',
-    redirectUri: 'https://fuzzer-dev.firebaseapp.com/login'
+    redirectUri: 'https://fuzzer.app/login'
   }
 };
