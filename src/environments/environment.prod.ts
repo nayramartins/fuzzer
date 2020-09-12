1b9cba1e22a23388167f1405e5707089c2c3d7bc
nayramartins <martinsg.nayra@gmail.com>
2020-09-12 20:46:26 -0300

    fix: add fallback route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   spotify: {
     clientId: 'b69d4477ec644c22a999afc386cc74b6',
     scopes: 'user-library-read user-top-read user-read-email playlist-modify-private',
-    redirectUri: 'https://dreamy-easley-27ca42.netlify.app/login'
+    redirectUri: 'https://fuzzer.netlify.app/login'
   }
 };
